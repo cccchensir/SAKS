bdc8563f4dacc86e8ae8db5188c7b9704d687141
cccchensir <32130609+cccchensir@users.noreply.github.com>
2023-03-18 11:05:44 +0800

    Add files via upload

---
+++ model_cls.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import copy
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def gram_schmidt(vectors):
+    """Gram-Schmidt process. Modified from https://stackoverflow.com/questions/48119473.
+    Parameters
+    ----------
+        vectors: 2D tensor - [v1, v2, ...]
+    """
+    basis = (vectors[0:1, :] / torch.norm(vectors[0:1, :]))
+    for i in range(1, vectors.size(0)):
+        v = vectors[i:i + 1, :]
+        w = v - torch.matmul(torch.matmul(v, basis.T), basis)
+        basis = torch.cat([basis, w / torch.norm(w)], dim=0)
+    return basis
+
+
+def knn(x, k):
+    inner = -2 * torch.matmul(x.transpose(2, 1), x)
+    xx = torch.sum(x ** 2, dim=1, keepdim=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]  # (batch_size, num_points, k)
+    return idx
+
+
+def get_graph_feature1(x, k=20, idx=None):
+    batch_size = x.size(0)
+    num_points = x.size(2)
+    x = x.view(batch_size, -1, num_points)
+    if idx is None:
+        idx = knn(x, k=k)  # (batch_size, num_points, k)
+        device = x.device
+
+        idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points
+
+        idx = idx + idx_base
+
+        idx = idx.view(-1)
+
+    _, num_dims, _ = x.size()
+
+    x = x.transpose(2,
+                    1).contiguous()  # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    feature = x.view(batch_size * num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    delta = feature - x
+    r = torch.abs(delta).max(dim=2, keepdim=True)[0]
+    feature = torch.cat((delta / (r + 0.000001), x), dim=3).permute(0, 3, 1, 2).contiguous()
+
+    return feature, idx
+
+
+def get_graph_feature(x, k=20, idx=None):
+    batch_size = x.size(0)
+    num_points = x.size(2)
+    x = x.view(batch_size, -1, num_points)
+    if idx is None:
+        idx = knn(x, k=k)  # (batch_size, num_points, k)
+        device = x.device
+
+        idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points
+
+        idx = idx + idx_base
+
+        idx = idx.view(-1)
+
+    _, num_dims, _ = x.size()
+
+    x = x.transpose(2,
+                    1).contiguous()  # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    feature = x.view(batch_size * num_points, -1)[idx, :]
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    feature = torch.cat((feature - x, x), dim=3).permute(0, 3, 1, 2).contiguous()
+
+    return feature, idx
+
+
+class SAKS(nn.Module):
+    def __init__(self, in_channels, out_channels, feat_channels, rank=4):
+        super(SAKS, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.feat_channels = feat_channels
+
+        self.conv0 = nn.Sequential()
+        basis = nn.init.orthogonal_(torch.empty(rank, out_channels * in_channels))
+        self.basis = nn.Parameter(basis, requires_grad=True)
+        mu = nn.init.kaiming_normal_(torch.empty(out_channels, in_channels), nonlinearity='relu').view(
+            out_channels * in_channels)
+        self.mu = nn.Parameter(mu, requires_grad=True)
+        self.bn0 = nn.BatchNorm2d(rank)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+
+        self.noise = nn.Sequential()
+
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2)
+
+    def forward(self, x, y):
+        pass
+
+
+class Net(nn.Module):
+    def __init__(self, args, output_channels=40):
+        super(Net, self).__init__()
+        self.args = args
+        self.k = args.k
+
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm1d(args.emb_dims)
+        self.conv5 = nn.Sequential(nn.Conv1d(512, args.emb_dims, kernel_size=1, bias=False),
+                                   self.bn5,
+                                   nn.LeakyReLU(negative_slope=0.2))
+        self.linear1 = nn.Linear(args.emb_dims * 2, 512, bias=False)
+        self.bn6 = nn.BatchNorm1d(512)
+        self.dp1 = nn.Dropout(p=args.dropout)
+        self.linear2 = nn.Linear(512, 256)
+        self.bn7 = nn.BatchNorm1d(256)
+        self.dp2 = nn.Dropout(p=args.dropout)
+        self.linear3 = nn.Linear(256, output_channels)
+
+        self.saks1 = SAKS(6, 64, 6)
+        self.saks2 = SAKS(6, 64, 64 * 2)
+        self.saks3 = SAKS(6, 128, 64 * 2)
+        self.saks4 = SAKS(6, 256, 128 * 2)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        points = x
+
+        x, idx = get_graph_feature(x, k=self.k)
+        p, _ = get_graph_feature(points, k=self.k, idx=idx)
+        x, corr_loss1 = self.saks1(p, x)
+        x1 = x.max(dim=-1, keepdim=False)[0]
+
+        x, idx = get_graph_feature(x1, k=self.k)
+        p, _ = get_graph_feature(points, k=self.k, idx=idx)
+        x, corr_loss2 = self.saks2(p, x)
+        x2 = x.max(dim=-1, keepdim=False)[0]
+
+        x, idx = get_graph_feature(x2, k=self.k)
+        p, _ = get_graph_feature(points, k=self.k, idx=idx)
+        x, corr_loss3 = self.saks4(p, x)
+        x3 = x.max(dim=-1, keepdim=False)[0]
+
+        x, idx = get_graph_feature(x3, k=self.k)
+        p, _ = get_graph_feature(points, k=self.k, idx=idx)
+        x, corr_loss4 = self.saks4(p, x)
+        x4 = x.max(dim=-1, keepdim=False)[0]
+
+        x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        x = self.conv5(x)
+        x1 = F.adaptive_max_pool1d(x, 1).view(batch_size, -1)
+        x2 = F.adaptive_avg_pool1d(x, 1).view(batch_size, -1)
+        x = torch.cat((x1, x2), 1)
+
+        x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
+        x = self.dp1(x)
+        x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2)
+        x = self.dp2(x)
+        x = self.linear3(x)
+        return x
